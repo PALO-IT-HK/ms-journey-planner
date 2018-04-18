28ac8a97ffc8558a6ae563c407097012154d25aa
Beda Tse <beda@coeus.hk>
2018-04-18 18:55:17 +0800

    remove sensitive information from src code

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   journey_planner_api_url: 'https://api.tfl.gov.uk/Journey/JourneyResults',
   endpointBaseUrl: process.env.ENDPOINT_BASEINTERFACE || 'localhost:3000',
-  app_id: '5ebfd576',
-  app_key: '019b1eb8b5ddd58804fe1cb884ce009b',
+  app_id: process.env.BIKE_APP_ID || '',
+  app_key: process.env.BIKE_APP_KEY || '',
   is_mock_data: false
 };
